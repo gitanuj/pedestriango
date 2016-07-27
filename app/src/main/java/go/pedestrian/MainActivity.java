51a0afc36e96666588070441cc66a1311740b39d
Tanuj Mittal <tanuj.183@gmail.com>
2016-07-26 18:03:04 -0700

    Merge branch 'master' into location
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/java/go/pedestrian/MainActivity.java

--- app/src/main/java/go/pedestrian/MainActivity.java
+++ app/src/main/java/go/pedestrian/MainActivity.java
@@ -1,12 +1,25 @@
 package go.pedestrian;
 
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SwitchCompat;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.widget.CompoundButton;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.ActivityRecognition;
+
+public class MainActivity extends AppCompatActivity
+        implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+
+    public GoogleApiClient mApiClient;
 
     private SwitchCompat switchCompat;
 
@@ -35,5 +48,32 @@ public class MainActivity extends AppCompatActivity {
 
     private void bindSwitch(boolean on) {
         switchCompat.setChecked(on);
+    }
+
+    private void registerActivityRecognitionReceiver() {
+        mApiClient = new GoogleApiClient.Builder(this)
+                .addApi(ActivityRecognition.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+        mApiClient.connect();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Intent intent = new Intent(this, ActivityRecognizedService.class);
+        PendingIntent pendingIntent = PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        ActivityRecognition.ActivityRecognitionApi.requestActivityUpdates(mApiClient, 3000, pendingIntent);
+        Log.d("MAIN", "onConnected Called");
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d("MAIN", "Google Client connection suspended");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d("MAIN", "Google Client connection failed");
     }
 }
